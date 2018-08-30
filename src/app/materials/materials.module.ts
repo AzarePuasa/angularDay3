dd561a4c7117c9b4de4dbaea16c8d61eb747dbc6
AzarePuasa <azarepuasa@azarepuasa.com>
2018-08-30 09:33:51 +0800

    update component

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatButtonModule} from '@angular/material/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
+const MODULES = [ MatInputModule, MatCardModule, MatToolbarModule, 
+  MatIconModule, MatRadioModule, MatButtonModule, BrowserAnimationsModule]
+
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: MODULES,
+  exports: MODULES,
   declarations: []
 })
+
 export class MaterialsModule { }
